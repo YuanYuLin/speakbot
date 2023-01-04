4e6b94854e11b5ead1a5d18d372550789c3ff4bc
YuanYuLin <yuanyu.lin@gmail.com>
2023-01-04 21:55:57 +0800

    update bot.

--- TTSPlayer.py
+++ TTSPlayer.py
@@ -16,6 +16,25 @@ def run_index(json_index, task_index):
       file_path = action["FILE"]
       speak(file_path)
 
+def run_with_task(json_index, tasks, cur_time):
+  for task in tasks :
+    task_time = task["TIME"]
+    if cur_time == task_time :
+      task_action = task["ACTION"]
+      for action_index in task_action :
+        run_index(json_index, action_index)
+
+def run_routine_task(json_index, routine_list, cur_time, weekday):
+  tasks = routine_list[weekday]["TASK"]
+  run_with_task(json_index, tasks, cur_time)
+
+def run_temperary_task(json_index, temperary_list, cur_time, cur_date):
+  for temp in temperary_list :
+    temp_date = temp["DATE"]
+    if cur_date == temp_date :
+      tasks = temp["TASK"]
+      run_with_task(json_index, tasks, cur_time)
+
 def loop(file_task, file_index):
   loop_go_next = True
   json_task = None
@@ -30,18 +49,13 @@ def loop(file_task, file_index):
   temperary_list = json_task["TEMPERARY"]
 
   today =  datetime.datetime.today()
-  hour = today.hour
-  minute = today.minute
   weekday = today.weekday()
-  print(weekday, hour, minute)
-  cur_time = "%02d:%02d" % (hour, minute)
-  tasks = routine_list[weekday]["TASK"]
-  for task in tasks :
-    task_time = task["TIME"]
-    if cur_time == task_time :
-      task_action = task["ACTION"]
-      for action_index in task_action :
-        run_index(json_index, action_index)
+  cur_date = "%d/%02d/%02d" % (today.year, today.month, today.day)
+  cur_time = "%02d:%02d" % (today.hour, today.minute)
+  #print(cur_date, cur_time)
+
+  run_routine_task(json_index, routine_list, cur_time, weekday)
+  run_temperary_task(json_index, temperary_list, cur_time, cur_date)
 
   conf = json_task["GLOBAL"]
   loop_go_next = conf["LOOP_GO_NEXT"]
